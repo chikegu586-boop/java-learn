8d6d387d8858ec4aa61d06f17dc67876689893d8
Lenovo <chikegu586@gmail.com>
2026-06-24 17:47:48 +0800

    初始版

---
+++ day01/code/03_关键字/KeyWord.java
@@ -0,0 +1,17 @@
+/*
+	�ؼ��֣���Java���Ը����ض�����ĵ���
+
+	�ؼ��ֵ��ص㣺�ؼ��ֶ���Сд��
+
+	�ؼ���ע������
+		A:goto��const��Ϊ�����ִ���,Ŀǰ����ʹ��
+		B:����Editplus�����ĸ߼����±�,��Թؼ������������ɫ��ǣ��ǳ�ֱ��
+*/
+class KeyWord {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+
+	/经济
+
+}
