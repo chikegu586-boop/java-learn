0a4680375b2816829c1109e15346a879b3e4ce05
Lenovo <chikegu586@gmail.com>
2026-06-25 12:10:08 +0800

    111

--- day1/FindSum.java
+++ day1/FindSum.java
@@ -2,7 +2,7 @@
 //使用for循环求1-100的和
 public class FindSum {
      public static void findSum() {
-        //int[] nums = {8, 6, 5, 5, 10, 1, 9, 7, 56, 9};
+        //int[] nums = {8, 6, 5, 5, 10, 1, 9, 7, 56, 9};1111
         int sum = 0;
         for (int i = 0; i <= 100; i++) {
             sum = sum + i;
